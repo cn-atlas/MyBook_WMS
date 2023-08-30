2731592b0a041f914995bfde3a59408e41b95993
qinfei <qinfei@hmzkjy.cn>
2023-08-30 11:08:59 +0800

    [add] 基本框架

---
+++ README.md
@@ -0,0 +1,15 @@
+# openWMS 教程 《Python WEB 企业及实战》 ——Django WMS 项目剖析
+
+
+
+## Usage 
+
+`make html`
+
+
+
+## Licensing
+
+![GPL v3](https://www.gnu.org/graphics/gplv3-with-text-136x68.png)
+
+The source code is licensed under GPL v3. License is available here.
--- source/conf.py
+++ source/conf.py
@@ -5,6 +5,11 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../src'))
+
 
 project = 'Python WEB 企业级实战'
 copyright = '2023, Atlas'
@@ -14,7 +19,10 @@ release = 'V0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx_rtd_theme',
+    # 'chinese_search'
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -26,3 +34,24 @@ language = 'zh_CN'
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
+# 换个好看的主题
+html_theme = 'sphinx_rtd_theme'
+
+
+html_theme_options = {
+    # 'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
+    # 'analytics_anonymize_ip': False,
+    # 'logo_only': False,
+    # 'display_version': True,
+    # 'prev_next_buttons_location': 'bottom',
+    # 'style_external_links': False,
+    # 'vcs_pageview_mode': '',
+    # 'style_nav_header_background': 'white',
+    # # Toc options
+    # 'collapse_navigation': True,
+    # 'sticky_navigation': True,
+    # 'navigation_depth': 4,
+    # 'includehidden': True,
+    # 'titles_only': False
+}
